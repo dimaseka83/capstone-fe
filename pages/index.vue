c867e80fa772bf1afe0d38a10c4df475a8e19063
dhimaseka <dimaseka83@gmail.com>
2022-11-17 16:49:59 +0700

    ++

--- pages/index.vue
+++ pages/index.vue
@@ -61,7 +61,7 @@ export default class IndexPage extends mixins(mix) {
             <p class="text--disabled" :class="nosm ? 'body' : 'body-2'">
               Cek siklus menstruasi kamu dan simpan hasilnya.
             </p>
-            <v-btn v-if="nosm" color="pink" large class="px-5 rounded-lg" dark>
+            <v-btn v-if="nosm" color="pink" x-large class="px-5 rounded-lg" dark>
               Daftar Sekarang
             </v-btn>
             <v-btn v-else color="pink" class="px-5 rounded-lg" dark>
